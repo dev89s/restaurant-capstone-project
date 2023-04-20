1c981f9cd03213a989c3ee38af798c073517f7db
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-20 15:06:28 +0300

    Add more unit tests for menuCount function

--- src/modules/menu-count.test.js
+++ src/modules/menu-count.test.js
@@ -1,18 +1,67 @@
 import { JSDOM } from 'jsdom';
-import menuCount from './menu-count';
+import menuCount from './menu-count.js';
 import getMeals from './get-meals.js';
 import getLikes from './get-likes.js';
 import loadList from './load-list.js';
 
-
 describe('test menuCount DOM function', () => {
   test('menuCount counts the DOM elements correctly', async () => {
     // Arrange
-    const dom = new JSDOM();;
+    const dom = new JSDOM();
     const { document } = dom.window;
     global.document = document;
     const mealContainers = document.createElement('div');
-    const itemCount = 0
+    const itemCount = 10;
+    for (let i = 0; i < itemCount; i += 1) {
+      const mealCont = document.createElement('div');
+      mealCont.classList.add('meal-container');
+      mealContainers.appendChild(mealCont);
+    }
+    const menuCounterEl = document.createElement('span');
+    menuCounterEl.classList.add('menu-counter');
+
+    document.body.appendChild(mealContainers);
+    document.body.appendChild(menuCounterEl);
+
+    // Act
+    menuCount();
+
+    // Assess
+    expect(menuCounterEl.textContent).toBe(`Menu(${itemCount})`);
+  });
+
+  test('menuCount Edge case of 0 elemets', async () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.document = document;
+    const mealContainers = document.createElement('div');
+    const itemCount = 0;
+    for (let i = 0; i < itemCount; i += 1) {
+      const mealCont = document.createElement('div');
+      mealCont.classList.add('meal-container');
+      mealContainers.appendChild(mealCont);
+    }
+    const menuCounterEl = document.createElement('span');
+    menuCounterEl.classList.add('menu-counter');
+
+    document.body.appendChild(mealContainers);
+    document.body.appendChild(menuCounterEl);
+
+    // Act
+    menuCount();
+
+    // Assess
+    expect(menuCounterEl.textContent).toBe(`Menu(${itemCount})`);
+  });
+
+  test('menuCount Edge case of big number of elemets', async () => {
+    // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.document = document;
+    const mealContainers = document.createElement('div');
+    const itemCount = 100000;
     for (let i = 0; i < itemCount; i += 1) {
       const mealCont = document.createElement('div');
       mealCont.classList.add('meal-container');
@@ -33,9 +82,12 @@ describe('test menuCount DOM function', () => {
 
   test('menuCount counts the DOM elements in real scenario', async () => {
     // Arrange
+    const dom = new JSDOM();
+    const { document } = dom.window;
+    global.document = document;
     await getMeals();
     await getLikes();
-    
+
     const mealList = document.createElement('div');
     mealList.classList.add('meal-list');
     document.body.appendChild(mealList);
@@ -45,8 +97,6 @@ describe('test menuCount DOM function', () => {
     menuCounter.textContent = 'Menu';
     document.body.appendChild(menuCounter);
 
-    console.log(localStorage.getItem('meals'));
-    console.log(localStorage.getItem('likes'));
     loadList();
     const menuCountEL = document.querySelector('.menu-counter');
 
