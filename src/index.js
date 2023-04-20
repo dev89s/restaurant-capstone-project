91e032b6bbfda6a54f9ec09b3270dbb82a271d89
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-20 12:14:21 +0300

    Rename mealCount to menCount in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import getMeals from './modules/get-meals.js';
 import loadList from './modules/load-list.js';
 import commentsPopup from './modules/popup.js';
 import getLikes from './modules/get-likes.js';
-import mealCount from './modules/meal-count.js';
+import menuCount from './modules/menu-count.js';
 
 const init = async () => {
   await getLikes();
   await getMeals();
   loadList();
   commentsPopup();
-  mealCount();
+  menuCount();
 };
 
 init();
