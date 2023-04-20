8aa1a6092a6a0066b2fa7f641b33a9755cc20b2a
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-20 12:02:31 +0300

    Menu counter added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import getMeals from './modules/get-meals.js';
 import loadList from './modules/load-list.js';
 import commentsPopup from './modules/popup.js';
 import getLikes from './modules/get-likes.js';
+import mealCount from './modules/meal-count.js';
 
 const init = async () => {
   await getLikes();
   await getMeals();
   loadList();
   commentsPopup();
+  mealCount();
 };
 
 init();
