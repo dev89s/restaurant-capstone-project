c9f3e794b5554945635ce8602d41378436b868e7
skabeo <spencerokyere95@gmail.com>
2023-04-20 11:58:54 +0100

    Add a comment counter module

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import loadList from './modules/load-list.js';
 import commentsPopup from './modules/popup.js';
 import getLikes from './modules/get-likes.js';
 import menuCount from './modules/menu-count.js';
+import commentCounter from './modules/commentCount';
 
 const init = async () => {
   await getLikes();
@@ -11,6 +12,7 @@ const init = async () => {
   loadList();
   commentsPopup();
   menuCount();
+  commentCounter();
 };
 
 init();
---
+++ src/modules/commentCount.js
@@ -0,0 +1,24 @@
+const commentCounter = () => {
+  const menuHolder = document.querySelectorAll('.meal-container');
+  menuHolder.forEach((meal) => {
+    meal.addEventListener('click', async (event) => {
+      const targetElement = event.target;
+      const comment = targetElement.textContent;
+
+      if (comment === 'Comments') {
+        const popup = document.querySelector('#displayPopup');
+        setTimeout(() => {
+          const commentSpans = popup.querySelectorAll('.comment-list');
+          const commentCount = commentSpans.length;
+          const commentHeader = popup.querySelector('#comment-header');
+          commentHeader.textContent = `Comments(${commentCount})`
+          console.log(commentCount);
+        }, 500);
+      }
+    });
+  });
+};
+
+
+
+export default commentCounter;
