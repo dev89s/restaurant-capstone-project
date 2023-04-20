45becaeeccc1c60f82128b6b2e0d1dd21d46e2ff
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-04-20 12:15:49 +0300

    Fix eslint error

--- src/modules/menu-count.js
+++ src/modules/menu-count.js
@@ -1,7 +1,7 @@
 const menuCount = () => {
   const mealContainers = document.querySelectorAll('.meal-container');
   const menuCounterEl = document.querySelector('.menu-counter');
-  menuCounterEl.textContent = `Menu(${mealContainers.length})`
-}
+  menuCounterEl.textContent = `Menu(${mealContainers.length})`;
+};
 
 export default menuCount;
